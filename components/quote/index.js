ec5027e7c83390bd0d291f7bfccad6b519df09ad
blaccgirl <kziboh@i4di.org>
2022-10-01 10:05:26 +0100

    resolve merge conflicts

--- components/quote/index.js
+++
@@ -1,9 +0,0 @@
-import styles from "./styles.module.css"
-export default function Quote(){
-    return (
-        <section className={styles.container}>
-            <span>Ready to work? </span>
-            <button>Get a Quote</button>
-        </section>
-    )
-}
